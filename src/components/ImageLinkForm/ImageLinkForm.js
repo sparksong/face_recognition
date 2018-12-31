fa6ac40fc82ebe576c9ef49c906a4fd8d662280c
Sparksong <justin.frederiksen@gmail.com>
2018-12-31 10:13:17 -0600

    Updating facial recognition app

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import './ImageLinkForm.css';
 
 const ImageLinkForm = () => {
-  return (
-    <div className='f3'>
-      <p>
-        { 'SmartGuy-san will detect faces in your pictures. Try it!' }
-      </p>
-      <div>
-        <input className='f4 pa2 w-70 center' type='text'/>
-        <button className='w-30 grow f4 ph3 pv2 dib white bg-light-purple'>
-          Detect
-        </button>
+    return (
+        <div className='f3'>
+        <p>
+          { 'Mr. SmartGuy will detect faces in your pictures. Try it!' }
+        </p>
+        <div className='center'>
+          <div className='form center pa4 br3 shadow-5'>
+            <input className='f4 pa2 w-70 center' type='text'/>
+            <button className='w-30 grow f4 ph3 pv2 dib white bg-light-purple'>
+              Detect
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    );
 }
 
 export default ImageLinkForm;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'Justin your current ranking: '}
+            </div>
+            <div className='white f1'>
+                {'#5'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
